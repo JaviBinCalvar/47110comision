03b3e7a4ac882873d89d16848611fd7e27fcaa3c
nelson bin calvar <javier_calvar@yahoo.com>
2023-11-15 23:50:07 -0300

    segundo trabajo JavaScript Bin Calvar Nelson

--- js/main.js
+++ js/main.js
@@ -1,22 +1,59 @@
 
 
-//variables
-let cantidad = parseInt(prompt("ingrese cantidad del producto seleccionado"));
-console.log(cantidad);
-let precio = parseFloat(prompt("ingrese el precio del producto"));
-console.log(precio);
+// funciones
 
-if ((cantidad !== 0) && (precio !== 0)){
-    let impuestos = precio*1.21;
-    let precioImpuesto = precio+impuestos; 
-    let compra = cantidad*precioImpuesto;
-    let descuento = compra*0.10;
-    let totalPagar = compra-descuento;
+function precioAnzuelos(precio){
+    let cantidad = parseInt(prompt("ingrese cantidad anzuelo Mustad"));
+    let preciofinal = precio * cantidad;
 
-    alert("El descuento es de: "+descuento);
-    alert("El total a pagar es de:"+totalPagar);
+        if(preciofinal > 500){
+            preciofinal * 0.9;
+        } else {
+            preciofinal;
+        }
 
-} else{
-    alert("Ingrese cantidad de productos y precio distinto de 0");
+    return preciofinal;
 }
 
+function precioPlumas(precio){
+    
+    let cantidad = parseInt(prompt("ingrese cantidad pluma Marabu"));
+    let preciofinal = precio * cantidad;
+    
+
+        if(preciofinal > 800){
+            preciofinal * 0.85;
+        } else {
+            preciofinal;
+        }
+
+    return preciofinal;
+}
+
+function calculoPrecio(producto, funcionPrecioFinal){
+
+    let preciofinal = funcionPrecioFinal(producto.precio);
+
+    return preciofinal;
+
+}
+
+//objetos
+
+class Producto{
+    constructor(nombre, precio, categoria){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+
+    }
+}
+
+const producto1 = new Producto("mustad", "100", "anzuelos");
+const producto2 = new Producto("marabu", "150", "plumas");
+
+const precioMustad = calculoPrecio(producto1, precioAnzuelos);
+const precioMarabu = calculoPrecio(producto2, precioPlumas);
+
+console.log(precioMustad);
+console.log(precioMarabu);
